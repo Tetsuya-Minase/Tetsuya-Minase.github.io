32efda3023ff69b50e88e241ffddb7c37b67cd03
Tetsuya Minase <tetsuya.minasen@gmail.com>
2020-03-18 23:42:13 +0900

    fix layout (#4)
    * feat: add library.📚
    
    * fix: remove unnecessary file.🧹
    
    * feat: add type definition.🐥
    
    * types: remove unnecessary comments.🧹
    
    * feat: add reset css in layout.🐥
    
    * feat: fix default pages.

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,25 +5,33 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
+export const seoQuery = graphql`
+  query SeoData {
+    site {
+      siteMetadata {
+        title
+        description
+        author
       }
-    `
-  )
+    }
+  }
+`
+
+type _Props = {
+  description: string
+  lang: string
+  meta: any[]
+  title: string
+}
+
+type Props = Partial<_Props>
+
+const SEO: React.FC<Props> = ({ description, lang, meta, title }) => {
+  const { site } = useStaticQuery(seoQuery)
 
   const metaDescription = description || site.siteMetadata.description
 
@@ -67,22 +75,15 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
+      ].concat(meta ?? [])}
     />
   )
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `ja`,
   meta: [],
   description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -1,32 +1,30 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from 'react'
+import { graphql } from 'gatsby'
 
-export default function Template({ data }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
+const Template: React.FC<any> = ({ data }) => {
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </div>
   )
 }
 
 export const pageQuery = graphql`
-    query($path: String!) {
-        markdownRemark(frontmatter: { path: { eq: $path } }) {
-            html
-            frontmatter {
-                date(formatString: "MMMM DD, YYYY")
-                path
-                title
-            }
-        }
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
     }
+  }
 `
+export default Template
