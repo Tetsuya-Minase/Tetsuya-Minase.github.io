e56d0dac75be22965b487b042c453d7d23f401de
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-03-21 16:45:34 +0900

    feat: add article tags.🏷

--- src/markdown-pages/sample.md
+++ src/markdown-pages/sample.md
@@ -1,8 +1,61 @@
 ---
-path: "/blog/first-post"
+path: "/blog/20200318"
 date: "2020-03-18"
 title: "My first blog post"
+tag: ["JavaScript", "TypeScript"]
 ---
 
-# title
-first post
+# はじめに
+マークダウンを読めるようになったので試す。
+
+# TL;DR.
+このページ
+
+# ヘディング
+## レベル２
+はこんなもん
+
+### レベル３
+はこれ
+
+#### レベル4
+だよ
+
+# リスト
+* hoge
+* huga
+* piyo
+
+# 順番付き
+1. first
+2. second
+3. third
+
+# コード系
+インライン`code`はこんな感じ  
+ブロックは↓みたいな  
+
+``` typescript
+const hoge = () => 'hoge';
+console.log(hoge());
+```
+
+# テーブル
+|title|value|
+|:---:|:----|
+|中央寄せの文字を入れている。|左寄せの値を入れてみている。|
+|２段目タイトル|２段目バリュー|
+
+# 画像
+![Gatsbyのアイコン](../images/gatsby-icon.png)
+
+## リサイズしたい時
+<img src="../images/gatsby-icon.png" width="100" height="100" alt="Gatsbyのアイコン">
+
+# 文字装飾
+**ボールド**  
+_イタリック_  
+
+# 引用
+> 引用してみた
+>> もっと
--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -1,29 +1,68 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
+import './blogTemplate.css'
+import styled from 'styled-components'
+
+const ArticleList = styled.ul`
+  display: flex;
+`
+const ArticleTag = styled.li<any>`
+  background-color: #f5f5f5;
+  display: inline-block;
+  margin: 0 10px;
+  position: relative;
+  &:before {
+    background-color: ${({ tagName }) => getTagColor(tagName)};
+    border-radius: 3px 0 0 3px;
+    content: '';
+    position: absolute;
+    left: -10px;
+    width: 10px;
+    height: 100%;
+  }
+`
+const getTagColor = (tag: string) => {
+  switch (tag) {
+    case 'JavaScript':
+      return '#f1e05a'
+    case 'TypeScript':
+      return '#2b7489'
+    default:
+      return '#315665'
+  }
+}
 
 const Template: React.FC<any> = ({ data }) => {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-      <article className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+      <article className="article">
+        <h1 className="article__title">{frontmatter.title}</h1>
+        <time className="article__time">{frontmatter.date}</time>
+        <ArticleList>
+          {frontmatter.tag.map((content: string, index: number) => (
+            <ArticleTag key={index} tagName={content}>
+              {content}
+            </ArticleTag>
+          ))}
+        </ArticleList>
+        <article className="article__content" dangerouslySetInnerHTML={{ __html: html }} />
       </article>
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
-  query($path: String!) {
+  query BlogTemplateData($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        tag
       }
     }
   }
