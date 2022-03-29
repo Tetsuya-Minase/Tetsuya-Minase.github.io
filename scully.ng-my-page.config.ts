388fd4f47dedf2357188c38b0e54302e30d87e43
Tetsuya Minase <tetsuya.minasen@gmail.com>
2022-03-29 23:22:37 +0900

    Update library (#68)
    * build: remove yarn.lock and add package-lock.json
    
    * build: use yarn.
    
    * build: update angular v12 to v13
    
    * build: update scully
    
    * build: fix scully warning.

--- scully.ng-my-page.config.ts
+++ scully.ng-my-page.config.ts
@@ -1,4 +1,7 @@
-exports.config = {
+import { ScullyConfig } from '@scullyio/scully';
+import '@scullyio/scully-plugin-puppeteer';
+
+export const config: ScullyConfig = {
   projectRoot: './src/app',
   outDir: './public',
   routes: {}
