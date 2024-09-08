f18819405979a79021cf59f57233d3fe96de811f
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2024-09-08 21:16:04 +0900

    build: fix build error.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { SeoService } from './common/service/seo/seo.service';
-import { FooterModule } from './common/component/footer/footer.module';
-import { TopModule } from './page/top/top.module';
-import { HeaderComponent } from './common/component/header/header.component';
-import { HeadingModule } from './common/atoms/heading/heading.module';
-import { LinksModule } from './page/links/links.module';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule, routes} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {SeoService} from './common/service/seo/seo.service';
+import {FooterModule} from './common/component/footer/footer.module';
+import {TopModule} from './page/top/top.module';
+import {HeaderComponent} from './common/component/header/header.component';
+import {HeadingModule} from './common/atoms/heading/heading.module';
+import {LinksModule} from './page/links/links.module';
 
-@NgModule({ declarations: [
-        AppComponent,
-        HeaderComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        FooterModule,
-        TopModule,
-        LinksModule,
-        HeadingModule], providers: [SeoService, provideClientHydration(), provideHttpClient(withInterceptorsFromDi())] })
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    FooterModule,
+    TopModule,
+    LinksModule,
+    HeadingModule
+  ],
+  providers: [
+    SeoService,
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+})
 export class AppModule {
 }
