dce2986fd64ac4677f8513a92a5d47defa5e706f
Tetsuya Minase <tetsuya.minasen@gmail.com>
2024-09-08 23:10:15 +0900

    update angular18 (#93)
    * build: update runtime.
    
    * build: update angular to v18.
    
    * build: remove unused libraries.
    
    * build: change deprecated libraries.
    
    * build: update libraries.
    
    * build: remove unused scripts.
    
    * refactor: remove unused files.
    
    * build: fix build error.
    
    * build: remove unused file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { TopComponent } from './page/top/component/top.component';
 import { LinksComponent } from './page/links/component/links.component';
 
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: TopComponent },
   { path: 'link', component: LinksComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {})],
+  imports: [RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
